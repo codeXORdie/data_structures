c6ff7c42483de19ed9330627387a019ea246df64
codeXORdie <34334448+codeXORdie@users.noreply.github.com>
2018-02-18 22:06:44 +0300

    Add files via upload

---
+++ IntStack.java
@@ -0,0 +1,23 @@
+
+public class IntStack {
+	
+	private int[] stack;
+	private int top;
+	private int size;
+	
+	public IntStack() {
+		this(50);
+	}
+	
+	public IntStack(int size) {
+		this.size = size;
+		top = -1;
+		stack = new int [size];
+	}
+	
+	public boolean push(int item) {
+		
+		
+	}
+
+}
